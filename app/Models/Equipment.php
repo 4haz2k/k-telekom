1ce51f508114be2b34387f2f062d0ebb4d7eef4b
Alex <zytia@mail.ru>
2023-02-01 23:41:30 +0300

    Добавил связь моделей

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Equipment extends Model
 {
     use HasFactory;
 
     protected $table = 'equipments';
+
+    /**
+     * Получение типа оборудования
+     *
+     * @return BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(EquipmentType::class, 'type_id');
+    }
 }
--- app/Models/EquipmentType.php
+++ app/Models/EquipmentType.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class EquipmentType extends Model
 {
     use HasFactory;
 
     protected $table = 'equipment_types';
+
+    /**
+     * Получение оборудований, привязанных к этому типу
+     *
+     * @return HasMany
+     */
+    public function equipments(): HasMany
+    {
+        return $this->hasMany(Equipment::class, "type_id");
+    }
 }
