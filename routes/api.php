08fa97da31b3893c33409610459838282f9e7363
Alex <zytia@mail.ru>
2023-02-02 13:00:37 +0300

    Определил методы для некоторых маршрутов

--- routes/api.php
+++ routes/api.php
@@ -1,32 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EquipmentController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::prefix('equipment')->middleware('auth:sanctum')->group(function () {
-    Route::get('/', [EquipmentController::class, ""]);
-    Route::post('/', [EquipmentController::class, ""]);
-    Route::get('/{id}', [EquipmentController::class, ""]);
-    Route::put('/{id}', [EquipmentController::class, ""]);
-    Route::delete('/{id}', [EquipmentController::class, ""]);
+    Route::get('/', [EquipmentController::class, "getList"])->name('equipment_list');
+    Route::post('/', [EquipmentController::class, ""])->name('equipment_insert');
+    Route::get('/{id}', [EquipmentController::class, "getEquipment"])->name('equipment_get');
+    Route::put('/{id}', [EquipmentController::class, ""])->name('equipment_edit');
+    Route::delete('/{id}', [EquipmentController::class, "deleteEquipment"])->name('equipment_delete');
 });
 
-Route::get('/equipment-type', [EquipmentController::class, ""])->middleware('auth:sanctum');
+Route::get('/equipment-type', [EquipmentController::class, "getTypesList"])->middleware('auth:sanctum');
 
-Route::post('/user/login');
+Route::post('/user/login', [AuthController::class, "loginUser"]);
